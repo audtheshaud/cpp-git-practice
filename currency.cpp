7ca75a5fb3cb6a445ea33fa7ee4bc73cad5bda88
AudTheShaud <abookernelson@gmail.com>
2024-07-04 21:32:42 -0400

    Final Copy

--- currency.cpp
+++ currency.cpp
@@ -2,36 +2,43 @@
 in US Dollars given currency amounts from three European countries  */
 
 #include <iostream>
-#include <iomanip>
 
 float conversion(float &currency, float conversion_rate){
-    currency *= conversion_rate;
+    /*
+    This function uses a conversion rate and the reference to the
+    user input currency amount to calculate the currency amount in
+    US Dollars
+    */
+    currency *= conversion_rate; 
 }
 
 
 int main(){
-    float euros, marks, francs;
+    float euros, marks, francs; // Instantiates floats of each currency
 
-    std::cout << "Enter the amount of German Euros: ";
-    std::cin >> euros;
+    std::cout << "Enter the amount of German Euros: "; // User prompt
+    std::cin >> euros; // User input
 
-    std::cout << "Enter the amount of Bosnian Marks: ";
-    std::cin >> marks;
+    std::cout << "Enter the amount of Bosnian Marks: "; // User prompt
+    std::cin >> marks; // User input
 
-    std::cout << "Enter the amount of Swiss Francs: ";
-    std::cin >> francs;
+    std::cout << "Enter the amount of Swiss Francs: "; // User prompt
+    std::cin >> francs; // User input
 
-    conversion(euros, 1.08);
+    conversion(euros, 1.08); 
     conversion(marks, 0.55);
     conversion(francs, 1.11);
 
-    float total = euros + marks + francs;
+    float total = euros + marks + francs; 
+    // Calculation of total in US Dollars
 
-    char str[40];
+    char str[40]; // Buffer for formatting
 
-    sprintf(str,"%.2f", total);
+    sprintf(str,"%.2f", total); 
+    // Formats to two decimal places
 
-    std::cout << "US Dollars = $" << str;
+    std::cout << "US Dollars = $" << str; 
+    // Outputs the Dollar amount to the terminal
     
-    return 0;
+    return 0; // End of function
 }
